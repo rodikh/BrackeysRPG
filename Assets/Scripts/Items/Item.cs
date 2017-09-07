cb504a6d00929c003c8a73c745258edb367a756b
rodikh <rodikh@gmail.com>
2017-09-07 23:01:20 +0300

    finished episode 09

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,4 +5,16 @@ public class Item : ScriptableObject {
     new public string name = "New Item";
     public Sprite icon = null;
     public bool isDefaultItem = false;
+
+    public virtual void Use () {
+        // use the item
+        // something might happen
+
+        Debug.Log("Using " + name);
+    }
+
+    public void RemoveFromInventory() {
+        Inventory.instance.Remove(this);
+
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
@@ -19,6 +20,11 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            return;
+        }
+
 		if (Input.GetMouseButtonDown(0)) {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
