ed992c8ab2e79c99afd47e682cab9080b42b75a2
rodikh <rodikh@gmail.com>
2017-09-07 10:24:14 +0300

    initial commit

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+    Transform target;
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        // TODO: turn this into a co-routine to run once every few frames and not every frame
+        if (target != null) {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    // Update is called once per frame
+    public void MoveToPoint(Vector3 point) {
+        agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget) {
+        agent.stoppingDistance = newTarget.radius * .8f;
+        agent.updateRotation = false;
+        target = newTarget.interactionTransform;
+    }
+
+    public void StopFollowingTarget() {
+        target = null;
+        agent.stoppingDistance = 0;
+        agent.updateRotation = true;
+    }
+
+    void FaceTarget() {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
