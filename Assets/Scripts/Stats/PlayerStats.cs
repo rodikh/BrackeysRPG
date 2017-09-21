c6df11a9f0c905fa38d45c62410801303faf0fec
rodikh <rodikh@gmail.com>
2017-09-21 13:22:32 +0300

    EP - 11 (Last) - Added CharacterCombat

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -21,4 +21,10 @@ public class PlayerStats : CharacterStats {
             damage.RemoveModifier(oldItem.damageModifier);
         }
     }
+
+    public override void Die() {
+        base.Die();
+        // Kill the player
+        PlayerManager.instance.KillPlayer();
+    }
 }
---
+++ Assets/Scripts/UI/CharacterSheet/CharacterSheetUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSheetUI : MonoBehaviour {
+
+    public Transform itemsParent;
+    public GameObject characterSheetUI;
+
+    EquipmentSlot[] slots;
+
+    EquipmentManager equipmentManager;
+
+    // Use this for initialization
+    void Start() {
+        equipmentManager = EquipmentManager.instance;
+        equipmentManager.onEquipmentChanged += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<EquipmentSlot>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("CharacterSheet")) {
+            characterSheetUI.SetActive(!characterSheetUI.activeSelf);
+        }
+    }
+
+    void UpdateUI(Equipment newItem, Equipment oldItem) {
+        //Debug.Log("Updating CS-UI");
+        for (int i = 0; i < slots.Length; i++) {
+            if (equipmentManager.currentEquipment[i] != null && !equipmentManager.currentEquipment[i].isDefaultItem) {
+                slots[i].AddItem(equipmentManager.currentEquipment[i]);
+            }
+            else {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
